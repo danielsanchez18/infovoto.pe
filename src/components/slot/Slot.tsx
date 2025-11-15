0d1a7b6e5ff8313873155917816aad4b776f008e
leoaguinaga <u21219471@utp.edu.pe>
2025-11-15 10:38:54 -0500

    chore: search and region dialogs on the navbar

--- src/components/slot/Slot.tsx
+++ src/components/slot/Slot.tsx
@@ -5,7 +5,7 @@ export default function Slot(props: SlotProps) {
 
   return (
     <div className='flex flex-col gap-2 jusitify-center text-center'>
-      <p className='h-22 w-32 bg-primary rounded-lg text-6xl text-white font-bold justify-center items-center flex'>
+      <p className='h-22 w-32 bg-black rounded-lg text-6xl text-white font-bold justify-center items-center flex'>
         {value}
       </p>
       <p className="capitalize">{label}</p>
