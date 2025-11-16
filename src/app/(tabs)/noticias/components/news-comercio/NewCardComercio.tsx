32da60b1efbdbd27cf075fce90796612d4e825d7
danielsanchez18 <dsanchez151r@gmail.com>
2025-11-16 16:25:12 -0500

    feat: enhance news components with dynamic data and improved layout

--- src/app/(tabs)/noticias/components/news-comercio/NewCardComercio.tsx
+++ src/app/(tabs)/noticias/components/news-comercio/NewCardComercio.tsx
@@ -1,20 +1,33 @@
 import Link from 'next/link'
 import React from 'react'
 
-export const NewCardComercio = () => {
+export type NewsComercio = {
+  id: string;
+  category: string;
+  categoryUrl: string;
+  title: string;
+  url: string;
+  imageUrl: string;
+};
+
+type NewCardComercioProps = {
+  news: NewsComercio;
+};
+
+export const NewCardComercio = ({ news }: NewCardComercioProps) => {
   return (
     <div className='grid gap-y-3'>
-      <Link href="/buscar/ELECCIONES-2026" 
-        className='uppercase font-bold text-sm text-primary hover:underline w-fit line-clamp-1'>
-        ELECCIONES 2026
-      </Link>
+  
+      <p className='uppercase font-bold text-sm text-primary w-fit line-clamp-1'>
+        {news.category}
+      </p>
       
-      <Link href="https://elcomercio.pe/politica/peru-primero-recicla-a-dos-candidatos-denunciados-por-agresion-martin-vizcarra-carlos-juscamayta-emilio-nicolich-tlcnota-noticia/"
+      <Link href={news.url}
         target="_blank" 
         className='grid grid-cols-2 gap-x-2 group'>
-        <h6 className='font-semibold text-balance line-clamp-6 leading-tight group-hover:underline group-hover:text-primary'>Elecciones 2026: Perú Primero recicla a dos candidatos denunciados por agresión</h6>
+        <h6 className='font-semibold text-balance line-clamp-6 leading-tight group-hover:underline group-hover:text-primary'>{news.title}</h6>
         <div className='w-full h-28 bg-gray-200 contain-content'>
-          <img src="https://elcomercio.pe/resizer/v2/SNLN6LUTWJGRZJFFX2IV7Q36TY.png?auth=a2598c364a0e241af789f28077b9dea74080102de0e954974c77f32fd99f2e75&width=320&height=180&quality=75&smart=true" alt="" 
+          <img src={news.imageUrl} alt={news.title} 
             className='object-cover w-full h-full'/>
         </div>
       </Link>
--- src/app/(tabs)/noticias/components/news-general/NewCardGeneralPrincipal.tsx
+++ src/app/(tabs)/noticias/components/news-general/NewCardGeneralPrincipal.tsx
@@ -2,13 +2,13 @@ import Link from "next/link"
 
 export const NewCardGeneralPrincipal = () => {
   return (
-    <Link href="">
+    <Link href="/noticias/86dc874a-1286-4d98-92f3-0f4b5978c185" target="_blank">
       <div className="h-[30rem] w-full relative overflow-hidden group">
         <img src="https://elcomercio.pe/resizer/v2/DBG3H6XICZEG7AU7KUU4ZJERTQ.jpg?auth=7b51a916fdc8d99547ced0df81872f4fc7e051ae6519a5cbb79b2a17fe86a5d4&width=1200&height=675&quality=75&smart=true" alt="" 
           className="object-cover w-full h-full absolute top-0 left-0"/>
         <div className="absolute bottom-5 left-5 w-[40%] bg-white flex flex-col justify-end to-transparent px-5 py-5">
-          <h2 className="text-2xl font-bold group-hover:underline group-hover:text-primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quos sed fugit.</h2>
-          <p className="mt-2 text-sm">Resumen breve de la noticia principal que proporciona una visión general del contenido.</p>
+          <h2 className="text-2xl font-bold group-hover:underline group-hover:text-primary">Los retos urgentes de las regiones del oriente de cara a las elecciones del 2026</h2>
+          <p className="mt-2 text-sm line-clamp-4">A menos de dos años de las elecciones del 2026, las regiones del oriente del país enfrentan una serie de desafíos que podrían influir de manera decisiva en la jornada electoral y en la agenda pública de los próximos gobiernos. Mandatarios locales, organizaciones sociales y expertos coinciden en que las problemáticas históricas se están combinando con nuevas tensiones sociales, económicas y ambientales.</p>
         </div>
       </div>
     </Link>
